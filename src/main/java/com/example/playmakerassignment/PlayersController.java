f7302bb2aa33069c487ddee68a729ad37bcb97f0
LiorInbar <liorinbar3@gmail.com>
2024-10-07 14:00:14 +0300

    add some comments

--- src/main/java/com/example/playmakerassignment/PlayersController.java
+++ src/main/java/com/example/playmakerassignment/PlayersController.java
@@ -7,6 +7,10 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class PlayersController {
 
+    /*
+    using post method and not get because the request has
+    a body.
+     */
     @PostMapping("/topN")
     public GetTopPlayersResponse getTopPlayers(@RequestBody GetTopPlayersRequest request) {
         return PlayersControllerImpl.getTopPlayers(request);
--- src/main/java/com/example/playmakerassignment/PlayersControllerImpl.java
+++ src/main/java/com/example/playmakerassignment/PlayersControllerImpl.java
@@ -11,19 +11,22 @@ import static java.util.stream.Collectors.groupingBy;
 
 public  class PlayersControllerImpl {
     public static GetTopPlayersResponse getTopPlayers(GetTopPlayersRequest request){
+        //mapping each player to the number of games he played
         Map<String,Long> playerToNumberOfAppearances =
                 request.participatedPlayers().stream()
-                .flatMap(l -> l.stream().distinct())
+                .flatMap(l -> l.stream().distinct()) //remove duplicates
                 .collect(groupingBy(Function.identity(),
                         Collectors.counting()));
 
-        List<String> res = playerToNumberOfAppearances.entrySet().stream()
+        //getting the top N players
+        List<String> res = playerToNumberOfAppearances.entrySet()
+                .stream()
                 .sorted(Comparator.comparing(player ->
-                        -player.getValue()))
-                .limit(request.requiredTopPlayers())
+                        -player.getValue()))  //using minus values to get reverse order
+                .limit(request.requiredTopPlayers()) // getting top N
                 .map(Map.Entry::getKey)
                 .toList();
 
-        return new GetTopPlayersResponse( res);
+        return new GetTopPlayersResponse(res);
     }
 }
